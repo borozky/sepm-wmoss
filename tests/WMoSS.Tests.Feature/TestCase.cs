d130d1f6ddb875b2d8e80d7bdb46ee48d08cfdcd
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-14 17:15:58 +1000

    Added helper classes and methods to generate fake entities

--- tests/WMoSS.Tests.Feature/TestCase.cs
+++ tests/WMoSS.Tests.Feature/TestCase.cs
@@ -10,12 +10,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
+using System.IO;
+using WMoSS.Entities;
+using Bogus;
+using System.Linq;
+using WMoSS.Tests.TestUtils;
 
 namespace WMoSS.Tests.Feature
 {
 
     // In razor page integration testing, tests will return a 500 response error
-    // without the following lines of code in .csproj file in the test project
+    // without the following lines of code in .csproj file in the WMoSS.Tests.Feature project
     //
     //<Target Name="CopyDepsFiles" AfterTargets="Build" Condition="'$(TargetFramework)'!=''">
     //  <ItemGroup>
@@ -33,9 +38,11 @@ namespace WMoSS.Tests.Feature
         protected readonly TestServer server;
         protected readonly HttpClient client;
 
+        private IEntityFactory factory;
+
         protected TestCase()
         {
-            var webfolder = @"C:\DEV\sepm-wmoss\src\WMoSS.Web";
+            var webfolder = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\..\src", @"WMoSS.Web");
             var webhostBuilder = new WebHostBuilder()
                 .UseEnvironment("Development")
                 .UseContentRoot(webfolder)
@@ -43,6 +50,13 @@ namespace WMoSS.Tests.Feature
 
             server = new TestServer(webhostBuilder);
             client = server.CreateClient();
+
+            factory = new ExcelEntityFactory();
+        }
+
+        protected void Define<T>(Func<T> definition) where T : class
+        {
+            factory.Define<T>(definition);
         }
 
         public void Dispose()
@@ -50,5 +64,60 @@ namespace WMoSS.Tests.Feature
             client.Dispose();
             server.Dispose();
         }
+
+        protected T Make<T>() where T : class
+        {
+            return Make<T>(1).FirstOrDefault();
+        }
+
+        protected IEnumerable<T> Make<T>(int numberOfEntities = 1) where T : class
+        {
+            return factory.Make<T>(numberOfEntities);
+        }
+
+        [Fact]
+        public void SmokeTest_EntityGeneration()
+        {
+            var id = 2;
+            factory.Define<Movie>(() =>
+            {
+                return new Faker<Movie>()
+                .RuleFor(m => m.Id, f => id++)
+                .RuleFor(m => m.Title, f => f.Lorem.Text())
+                .RuleFor(m => m.Genre, f => f.Lorem.Word())
+                .RuleFor(m => m.ReleaseYear, f => f.Random.Number(2017, 2019))
+                .RuleFor(m => m.RuntimeMinutes, f => f.Random.Number(100, 200))
+                .RuleFor(m => m.Description, f => f.Lorem.Paragraph())
+                .RuleFor(m => m.PosterFileName, f => f.Image.Image())
+                .RuleFor(m => m.Classification, f => f.PickRandom("G", "PG", "MA", "MA15+", "R18+", "X18+"))
+                .Generate();
+            });
+
+            var movie = factory.Make<Movie>();
+            Assert.NotNull(movie);
+        }
+
+        [Fact]
+        public void SmokeTest_MultipleEntityGeneration()
+        {
+            var id = 2;
+            factory.Define<Movie>(() =>
+            {
+                return new Faker<Movie>()
+                .RuleFor(m => m.Id, f => id++)
+                .RuleFor(m => m.Title, f => f.Lorem.Text())
+                .RuleFor(m => m.Genre, f => f.Lorem.Word())
+                .RuleFor(m => m.ReleaseYear, f => f.Random.Number(2017, 2019))
+                .RuleFor(m => m.RuntimeMinutes, f => f.Random.Number(100, 200))
+                .RuleFor(m => m.Description, f => f.Lorem.Paragraph())
+                .RuleFor(m => m.PosterFileName, f => f.Image.Image())
+                .RuleFor(m => m.Classification, f => f.PickRandom("G", "PG", "MA", "MA15+", "R18+", "X18+"))
+                .Generate();
+            });
+
+            var movies = factory.Make<Movie>(2);
+            Assert.Equal(movies.Count(), 2);
+        }
     }
+    
 }
---
+++ tests/WMoSS.Tests/TestUtils/IEntityFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WMoSS.Tests.TestUtils
+{
+    public interface IEntityFactory
+    {
+        /// <summary>
+        /// Defines how entities will be generated. Requires a callback.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="factory"></param>
+        void Define<T>(Func<T> factory) where T : class;
+
+        /// <summary>
+        /// Generates a fake entity. Entity's property values will depend on
+        /// how it is defined in Define() method
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T Make<T>() where T : class;
+
+        /// <summary>
+        /// Generates multiple entities. Entity's property values will depend on
+        /// how iti is defined in Define() method
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="numOfEntities"></param>
+        /// <returns></returns>
+        IEnumerable<T> Make<T>(int numOfEntities) where T : class;
+    }
+}
