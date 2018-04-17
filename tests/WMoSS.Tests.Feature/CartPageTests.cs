fe79284380925c591c4bed43e92caf1d4d66e0b5
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-17 23:29:54 +1000

    Setup basic integration test for cart page

---
+++ tests/WMoSS.Tests.Feature/CartPageTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WMoSS.Tests.Feature
+{
+    public class CartPageTests : TestCase
+    {
+        public CartPageTests() : base()
+        {
+
+        }
+
+        [Fact]
+        public async Task Test_CartPage_Works()
+        {
+            var response = await client.GetAsync("/Cart");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
