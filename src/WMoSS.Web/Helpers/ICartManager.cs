04756b09a1161e20e7d15fedd836effeb2397451
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-17 21:04:49 +1000

    Cart manager

---
+++ src/WMoSS.Web/Helpers/ICartManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WMoSS.Entities;
+
+namespace WMoSS.Web.Helpers
+{
+    public interface ICartManager
+    {
+        bool AddToCart(CartItem cartItem);
+        bool AddMultipleItemsToCart(IEnumerable<CartItem> cartItems);
+        bool AddToCart(int movieSessionId, int numTickets);
+        bool RemoveFromCart(int movieSessionId);
+        bool ModifyNumTickets(int movieSessionId, int numTickets);
+        Cart GetCart();
+    }
+}
--- src/WMoSS.Web/Pages/Cart/Index.cshtml.cs
+++ src/WMoSS.Web/Pages/Cart/Index.cshtml.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WMoSS.Entities;
+using WMoSS.Web.Helpers;
+using Microsoft.AspNetCore.Http;
+using WMoSS.Web.Extensions;
 
 namespace WMoSS.Web.Pages.Cart
 {
@@ -12,6 +16,46 @@ namespace WMoSS.Web.Pages.Cart
         public void OnGet()
         {
 
+        }
+    }
+
+    public class CartManager : ICartManager
+    {
+        private ISession _session;
+
+        public CartManager(ISession session)
+        {
+            _session = session;
+        }
+
+        public bool AddToCart(CartItem cartItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddToCart(int movieSessionId, int numTickets)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddMultipleItemsToCart(IEnumerable<CartItem> cartItems)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entities.Cart GetCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ModifyNumTickets(int movieSessionId, int numTickets)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveFromCart(int movieSessionId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
