91b83e8eff9fd9e79defeacd216269fba3fac99a
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-18 00:20:18 +1000

    Refactored model factories

--- tests/WMoSS.Tests.Feature/TestCase.cs
+++ tests/WMoSS.Tests.Feature/TestCase.cs
@@ -52,6 +52,8 @@ namespace WMoSS.Tests.Feature
             client = server.CreateClient();
 
             factory = new ExcelEntityFactory();
+
+            DefineEntities();
         }
 
         protected void Define<T>(Func<T> definition) where T : class
@@ -117,6 +119,53 @@ namespace WMoSS.Tests.Feature
 
             var movies = factory.Make<Movie>(2);
             Assert.Equal(movies.Count(), 2);
+        }
+
+        private void DefineEntities()
+        {
+            int theaterId = 2;
+            Define<Theater>(() =>
+            {
+                return new Faker<Theater>()
+                .RuleFor(t => t.Id, f => theaterId++)
+                .RuleFor(t => t.Name, f => $"Theater #{theaterId - 2}")
+                .RuleFor(t => t.Capacity, f => 50)
+                .RuleFor(t => t.Address, f => "123 Lygon Street, Melbourne")
+                .Generate();
+            });
+
+            int movieId = 2;
+            Define<Movie>(() =>
+            {
+                return new Faker<Movie>()
+                .RuleFor(m => m.Id, f => movieId++)
+                .RuleFor(m => m.Title, f => f.Lorem.Text())
+                .RuleFor(m => m.Genre, f => f.Lorem.Word())
+                .RuleFor(m => m.ReleaseDate, f => DateTime.UtcNow.AddDays(f.Random.Int(1, 365)))
+                .RuleFor(m => m.RuntimeMinutes, f => f.Random.Number(100, 200))
+                .RuleFor(m => m.Description, f => f.Lorem.Paragraph())
+                .RuleFor(m => m.PosterFileName, f => f.Image.Image())
+                .RuleFor(m => m.Classification, f => f.PickRandom("G", "PG", "MA", "MA15+", "R18+", "X18+"))
+                .Generate();
+            });
+
+            int movieSessionId = 2;
+            Define<MovieSession>(() =>
+            {
+                Random random = new Random();
+                int days = random.Next(1, 4);
+                int hour = random.Next(1, 4);
+                DateTime schedule = DateTime.Today.AddDays(days);
+                schedule.AddHours(hour * 4.5);
+                string scheduleStr = schedule.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+
+                return new Faker<MovieSession>()
+                .RuleFor(ms => ms.Id, f => movieSessionId++)
+                .RuleFor(ms => ms.TicketPrice, f => 20.00)
+                .RuleFor(ms => ms.ScheduledAt, f => DateTime.UtcNow.AddDays(f.Random.Int(1, 365)))
+                .RuleFor(ms => ms.TheaterId, f => f.Random.Int(2, 6))
+                .Generate();
+            });
         }
     }
     
