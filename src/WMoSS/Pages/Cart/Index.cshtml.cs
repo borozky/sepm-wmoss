0c307391b89bb77203be60e4ffedf7e2b532714f
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-19 09:40:46 +1000

    Add to cart for in movie details page

--- src/WMoSS/Pages/Cart/Index.cshtml.cs
+++ src/WMoSS/Pages/Cart/Index.cshtml.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WMoSS.Data;
+using WMoSS.Entities;
 
 namespace WMoSS.Pages.Cart
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public IndexModel(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public void OnGet()
         {
 
         }
+
+        [BindProperty]
+        public CartItem CartItem { get; set; }
+
+        public IActionResult OnPost()
+        {
+            return new JsonResult(CartItem);
+        }
+
     }
 }
--- src/WMoSS/Pages/Movies/Details.cshtml.cs
+++ src/WMoSS/Pages/Movies/Details.cshtml.cs
@@ -4,14 +4,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WMoSS.Data;
+using WMoSS.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WMoSS.Pages.Movies
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _dbcontext;
 
+        public DetailsModel(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        
+        public Movie Movie { get; private set; }
+        public CartItem CartItem { get; set; }
+        public IEnumerable<MovieSession> MovieSessions { get; private set; }
+        public List<SelectListItem> MovieSessionsOptions { get; private set; }
+
+        public async Task<IActionResult> OnGet(int id, CancellationToken ct)
+        {
+            Movie = await _dbcontext.Movies
+                .Include(m => m.MovieSessions)
+                    .ThenInclude(ms => ms.Theater)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id, ct);
+            
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+
+            MovieSessions = Movie.MovieSessions;
+            MovieSessionsOptions = MovieSessions.Select(ms => new SelectListItem
+            {
+                Value = ms.Id.ToString(),
+                Text = String.Format("{0:ddd dd MMM yyyy hh:mm tt} at {1}", ms.ScheduledAt, ms.Theater.Name)
+            }).ToList();
+            return Page();
         }
     }
 }
