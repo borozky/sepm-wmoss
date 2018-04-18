ba6f6480c596b9a29838b15c9303294094b4105d
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-18 19:14:17 +1000

    Process cart

--- src/WMoSS.Web/Pages/Cart/Index.cshtml.cs
+++ src/WMoSS.Web/Pages/Cart/Index.cshtml.cs
@@ -9,6 +9,7 @@ using WMoSS.Web.Helpers;
 using Microsoft.AspNetCore.Http;
 using WMoSS.Web.Extensions;
 using WMoSS.Repositories;
+using System.ComponentModel.DataAnnotations;
 
 namespace WMoSS.Web.Pages.Cart
 {
@@ -46,5 +47,36 @@ namespace WMoSS.Web.Pages.Cart
                 movieSessionRepository,
                 theaterRepository);
         }
+
+
+        [BindProperty]
+        public CartItemRequest AddToCartRequest { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            SetupCartManager();
+            int movieSessionId = AddToCartRequest.MovieSessionId.Value;
+            int ticketQty = AddToCartRequest.TicketQty.Value;
+
+            cartManager.AddToCart(movieSessionId, ticketQty);
+
+            return RedirectToPage();
+        }
+    }
+
+    public class CartItemRequest
+    {
+        [Required]
+        [Range(2, int.MaxValue)]
+        public int? MovieSessionId { get; set; }
+
+        [Required]
+        [Range(1, 50)]
+        public int? TicketQty { get; set; }
     }
 }
