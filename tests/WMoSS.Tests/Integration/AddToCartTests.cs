4a7867325c2419a333f33a486cc75aa193bc8754
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-23 10:42:16 +1000

    Added integration test for add to cart feature

---
+++ tests/WMoSS.Tests/Integration/AddToCartTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WMoSS.Data;
+using WMoSS.Entities;
+using Xunit;
+
+namespace WMoSS.Tests.Integration
+{
+    public class AddToCartTests : TestFixture<Startup>
+    {
+        ApplicationDbContext db;
+        HttpClient client;
+
+        public AddToCartTests() : base()
+        {
+            client = Client;
+            db = Server.Host.Services.GetService(typeof(ApplicationDbContext)) as ApplicationDbContext;
+            DbInitializer.Initialize(db);
+        }
+
+        [Fact]
+        public async Task Test_AddToCart_Works()
+        {
+            var latestMovieSession = await db.MovieSessions.FirstOrDefaultAsync();
+            var tickets = 2;
+
+            var data = new Dictionary<string, string>
+            {
+                [$"{nameof(CartItem)}.{nameof(CartItem.MovieSessionId)}"] = latestMovieSession.Id.ToString(),
+                [$"{nameof(CartItem)}.{nameof(CartItem.TicketQuantity)}"] = tickets.ToString()
+            };
+
+            var formData = await Utilities.GetRequestContentAsync(client, $"/Movies/Details/{latestMovieSession.MovieId}", data);
+
+            var response = await client.PostAsync("./Cart/?handler=AddToCart", formData);
+            var content = await response.Content.ReadAsStringAsync();
+            
+            Assert.Equal("/Cart", response.Headers.Location.ToString());
+        }
+    }
+}
