1459ca8b203de7bec9d5f030fbebecf7433a944d
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-25 18:57:42 +1000

    Added testcase superclass

---
+++ tests/WMoSS.Tests/Unit/TestCase.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WMoSS.Data;
+
+namespace WMoSS.Tests.Unit
+{
+    public abstract class TestCase : IDisposable
+    {
+        protected DbContextOptions<ApplicationDbContext> dbContextOptions;
+        protected Mock<ApplicationDbContext> mockDbContext;
+        protected ApplicationDbContext db;
+        protected Mock<ISession> mockSession;
+        protected ISession session;
+        protected HttpContext httpContext;
+        protected ActionContext actionContext;
+        protected IModelMetadataProvider modelMetadataProvider;
+        protected ModelStateDictionary modelState;
+        protected ViewDataDictionary viewData;
+        protected TempDataDictionary tempData;
+        protected PageContext pageContext;
+
+        protected TestCase()
+        {
+            dbContextOptions = Utilities.TestingDbContextOptions<ApplicationDbContext>();
+            mockDbContext = new Mock<ApplicationDbContext>(dbContextOptions);
+            db = mockDbContext.Object;
+            mockSession = new Mock<ISession>();
+            session = mockSession.Object;
+            httpContext = new DefaultHttpContext
+            {
+                Session = session
+            };
+            modelState = new ModelStateDictionary();
+            actionContext = new ActionContext(httpContext, new Microsoft.AspNetCore.Routing.RouteData(), new PageActionDescriptor(), modelState);
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData
+            };
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
