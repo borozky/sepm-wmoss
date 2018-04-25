79adefeef964b96e976b3fab5c6e1df5a645c724
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-04-25 19:11:43 +1000

    Add to cart razor page model unit tests

---
+++ tests/WMoSS.Tests/Unit/Cart/AddToCartTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace WMoSS.Tests.Unit.Cart
+{
+    public class AddToCartTests : TestCase
+    {
+        private Pages.Cart.IndexModel CartIndexModel;
+
+        public AddToCartTests()
+        {
+            CartIndexModel = new Pages.Cart.IndexModel(db)
+            {
+                PageContext = pageContext,
+                TempData = tempData,
+                Url = new UrlHelper(pageContext)
+            };
+        }
+
+        [Fact]
+        public void Test_WhenAddToCartIsSuccessful_RedirectsToCartPage()
+        {
+            // ARRANGE
+            CartIndexModel.CartItem = new Mock<Entities.CartItem>().Object;
+
+            // ACT
+            var result = CartIndexModel.OnPostAddToCart();
+
+            // ASSERT
+            var redirect = Assert.IsType<RedirectToPageResult>(result);
+            Assert.Equal("/Cart/Index", redirect.PageName);
+        }
+
+        [Fact]
+        public void Test_WhenAddToCartHasModelStateErrors_ItRedirectsBasedOnReturnUrl()
+        {
+            // ARRANGE
+            CartIndexModel.CartItem = new Mock<Entities.CartItem>().Object;
+            CartIndexModel.ReturnUrl = "/Index";
+            CartIndexModel.ModelState.AddModelError("", "Invalid");
+
+            // ACT
+            var result = CartIndexModel.OnPostAddToCart();
+
+            // ASSERT
+            var redirect = Assert.IsType<RedirectResult>(result);
+            Assert.Equal(CartIndexModel.ReturnUrl, redirect.Url);
+        }
+    }
+}
