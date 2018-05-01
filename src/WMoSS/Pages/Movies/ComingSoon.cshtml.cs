8e434c0c5f29eb831d2c4db0aa80aa7bad6085df
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-05-01 23:25:50 +1000

    Server side code for 'Coming Soon' page

--- src/WMoSS/Pages/Movies/ComingSoon.cshtml.cs
+++ src/WMoSS/Pages/Movies/ComingSoon.cshtml.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WMoSS.Data;
+using WMoSS.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace WMoSS.Pages.Movies
 {
     public class ComingSoonModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _dbcontext;
 
+        public ComingSoonModel(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public IEnumerable<Movie> Movies { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Movies = await _dbcontext.Movies
+                .AsNoTracking()
+                .Where(m => m.MovieSessions.Count() == 0)
+                .Where(m => m.ReleaseDate != null)
+                .Where(m => DateTime.Now.AddMonths(3) > m.ReleaseDate.Value)
+                .ToListAsync();
         }
     }
 }
