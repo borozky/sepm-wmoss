18099cc890c8bf69e492e95c57e6c3210525acee
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-05-01 23:39:24 +1000

    Added backend code for now-showing movies

--- src/WMoSS/Pages/Movies/NowShowing.cshtml.cs
+++ src/WMoSS/Pages/Movies/NowShowing.cshtml.cs
@@ -4,14 +4,50 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WMoSS.Data;
+using Microsoft.EntityFrameworkCore;
+using WMoSS.Entities;
 
 namespace WMoSS.Pages.Movies
 {
     public class NowShowingModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _dbcontext;
 
+        public NowShowingModel(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public IEnumerable<Movie> Movies { get; set; }
+
+        public async Task OnGetAsync([FromQuery] string sort)
+        {
+            Movies = await _dbcontext.Movies
+                .Include(m => m.MovieSessions)
+                .Where(m => m.MovieSessions.Count() > 0)
+                .AsNoTracking()
+                .ToListAsync();
+
+            if (Movies != null && Movies.Count() > 0)
+            {
+                switch (sort)
+                {
+                    case "title-a-z":
+                        Movies = Movies.OrderBy(m => m.Title);
+                        break;
+                    case "title-z-a":
+                        Movies = Movies.OrderByDescending(m => m.Title);
+                        break;
+                    case "rating":
+                        Movies = Movies.OrderByDescending(m => m.Rating);
+                        break;
+                    case "popular":
+                        // TODO
+                        break;
+                    
+                }
+            }
         }
     }
 }
