b892421b2e9fd072205e0090a6a589ef2fefa010
Melika <s3600143@student.rmit.edu.au>
2018-05-05 14:31:33 +1000

    refined home & order summary pages

--- mockups/src/js/home/ExpressBooking.js
+++ mockups/src/js/home/ExpressBooking.js
@@ -15,25 +15,85 @@ class ExpressBooking extends Component {
                 {id: 4, name: "Theater #4", address: "123 Lygon St., Melbourne", capacity: 50},
                 {id: 5, name: "Theater #5", address: "123 Lygon St., Melbourne", capacity: 50}
             ],
-            movies: [],
-            sessions: []
+            movies: [
+                {id: 1, name: "Movie #1", rating: "R", price: 20},
+                {id: 2, name: "Movie #2", rating: "R", price: 20},
+                {id: 3, name: "Movie #3", rating: "R", price: 20},
+                {id: 4, name: "Movie #4", rating: "R", price: 20},
+                {id: 5, name: "Movie #5", rating: "R", price: 20}
+            ],
+            sessions: [
+                {id: 1, day: "Monday ", time: "4:30", date: 30/1},
+                {id: 2, day: "Monday ", time: "4:30", date: 30/1},
+                {id: 3, day: "Monday ", time: "4:30", date: 30/1},
+                {id: 4, day: "Monday ", time: "4:30", date: 30/1},
+                {id: 5, day: "Monday ", time: "4:30", date: 30/1}
+            ]
         }
+     
     }
+    
+ 
 
     render() {
         return (
+
             <form action="" method="POST">
-                <h4>Express Checkout</h4>
+            <div class="row">
+            <div class="col-sm-2 col-md-2">
                 <label htmlFor="ExpressCheckout-theater">Select Theater</label><br/>
-                <select name="theater" id="ExpressCheckout-theater">
+                <select name="theater" id="ExpressCheckout-theater" className="form-control">
+                <option selected="selected">select a theater</option>
                 {
                     this.state.theaters.map((theater, index) => (
                         <option key={index} value={theater.id}>{theater.name}</option>
                     ))
                 }
                 </select>
-                { /* the rest goes here */ }
+                </div>
+
+                <div class="col-sm-2 col-md-2">
+                <label htmlFor="ExpressCheckout-movie">Select Movie</label><br/>
+                <select name="movie" id="ExpressCheckout-movie" className="form-control col-sm-3 col-md-3">
+                <option selected="selected">select a movie</option>
+                {
+                    this.state.movies.map((movie, index) => (
+                        <option key={index} value={movie.id}>{movie.name}</option>
+                    ))
+                }
+                </select>
+                </div>
+
+                <div class="col-sm-2 col-md-2">
+                <label htmlFor="ExpressCheckout-session">Select Session</label><br/>
+                <select name="movie" id="ExpressCheckout-session" className="form-control col-sm-3 col-md-3">
+                <option selected="selected">select a session</option>
+                {
+                    this.state.sessions.map((session, index) => (
+                        <option key={index} value={session.id}>{session.day}{session.time}</option>
+                    ))
+                }
+                </select>
+                </div>
+
+                <div class="col-sm-1 col-md-1">
+                    <label htmlFor="ExpressCheckout-ticket">Tickets</label><br/>
+                     <input type="number" name="ticket" className="form-control" min="1" placeholder="Select"/>
+                </div>
+                <div class="col-sm-2 col-md-2">
+                    <label htmlFor="ExpressCheckout-seat">Select Seats</label><br/>
+                     <button className="form-control btn btn-default">Select from the map</button>
+                </div>
+                </div>
+                <br/>
+                <div class="row">
+                <div class="col-sm-2 col-md-2 pull-right">
+                    <input type="submit" name="submit" value="Add To Cart" className="form-control btn btn-primary"/>
+                </div>
+
+                </div>
             </form>
+           
         )
     }
 
