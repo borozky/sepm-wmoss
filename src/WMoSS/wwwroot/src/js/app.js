cd29d242ae127a0c6e9a86664c43070958d0b645
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-05-06 12:41:12 +1000

    Added JS and scss files

--- src/WMoSS/wwwroot/src/js/app.js
+++ src/WMoSS/wwwroot/src/js/app.js
@@ -2,3 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'jquery-validation';
 import 'jquery-validation-unobtrusive';
+
+import "./home/index";
+import "./movie-details/index";
+import "./search-results/index";
+import "./now-showing/index";
+import "./coming-soon/index";
+import "./cart/index";
+import "./checkout/index";
+import "./order-summary/index";
