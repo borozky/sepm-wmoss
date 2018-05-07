e3c3edfeeedc4645387d5abef90cba61a8dd7cb3
Melika <s3600143@student.rmit.edu.au>
2018-05-07 14:02:24 +1000

    seat allocation pop up

--- mockups/src/js/home/ExpressBooking.js
+++ mockups/src/js/home/ExpressBooking.js
@@ -37,63 +37,67 @@ class ExpressBooking extends Component {
 
     render() {
         return (
+            <div>
+            <div className="row">
 
-            <form action="" method="POST">
-            <div class="row">
-            <div class="col-sm-2 col-md-2">
-                <label htmlFor="ExpressCheckout-theater">Select Theater</label><br/>
-                <select name="theater" id="ExpressCheckout-theater" className="form-control">
-                <option selected="selected">select a theater</option>
-                {
-                    this.state.theaters.map((theater, index) => (
-                        <option key={index} value={theater.id}>{theater.name}</option>
-                    ))
-                }
-                </select>
+                <div className="col-sm-2 col-md-2">
+                    <label htmlFor="ExpressCheckout-theater">Select Theater</label><br/>
+                    <select name="theater" id="ExpressCheckout-theater" className="form-control">
+                    <option selected="selected">select a theater</option>
+                    {
+                        this.state.theaters.map((theater, index) => (
+                            <option key={index} value={theater.id}>{theater.name}</option>
+                        ))
+                    }
+                    </select>
                 </div>
 
-                <div class="col-sm-2 col-md-2">
-                <label htmlFor="ExpressCheckout-movie">Select Movie</label><br/>
-                <select name="movie" id="ExpressCheckout-movie" className="form-control col-sm-3 col-md-3">
-                <option selected="selected">select a movie</option>
-                {
-                    this.state.movies.map((movie, index) => (
-                        <option key={index} value={movie.id}>{movie.name}</option>
-                    ))
-                }
-                </select>
+                <div className="col-sm-2 col-md-2">
+                    <label htmlFor="ExpressCheckout-movie">Select Movie</label><br/>
+                    <select name="movie" id="ExpressCheckout-movie" className="form-control col-sm-3 col-md-3">
+                    <option selected="selected">select a movie</option>
+                    {
+                        this.state.movies.map((movie, index) => (
+                            <option key={index} value={movie.id}>{movie.name}</option>
+                        ))
+                    }
+                    </select>
                 </div>
 
-                <div class="col-sm-2 col-md-2">
-                <label htmlFor="ExpressCheckout-session">Select Session</label><br/>
-                <select name="movie" id="ExpressCheckout-session" className="form-control col-sm-3 col-md-3">
-                <option selected="selected">select a session</option>
-                {
-                    this.state.sessions.map((session, index) => (
-                        <option key={index} value={session.id}>{session.day}{session.time}</option>
-                    ))
-                }
-                </select>
+                <div className="col-sm-2 col-md-2">
+                    <label htmlFor="ExpressCheckout-session">Select Session</label><br/>
+                    <select name="movie" id="ExpressCheckout-session" className="form-control col-sm-3 col-md-3">
+                    <option selected="selected">select a session</option>
+                    {
+                        this.state.sessions.map((session, index) => (
+                            <option key={index} value={session.id}>{session.day}{session.time}</option>
+                        ))
+                    }
+                    </select>
                 </div>
 
                 <div class="col-sm-1 col-md-1">
                     <label htmlFor="ExpressCheckout-ticket">Tickets</label><br/>
                      <input type="number" name="ticket" className="form-control" min="1" placeholder="Select"/>
                 </div>
+
                 <div class="col-sm-2 col-md-2">
                     <label htmlFor="ExpressCheckout-seat">Select Seats</label><br/>
-                     <button className="form-control btn btn-default">Select from the map</button>
+                     <button id="selectSeats" className="form-control btn btn-default" data-toggle="modal" data-target="#SeatAllocationModal">
+                        Select from the map
+                    </button>
                 </div>
+
                 </div>
                 <br/>
-                <div class="row">
-                <div class="col-sm-2 col-md-2 pull-right">
+                <div className="row">
+                <div className="col-sm-2 col-md-2 pull-right">
                     <input type="submit" name="submit" value="Add To Cart" className="form-control btn btn-primary"/>
                 </div>
-                </div>
-            </form>
+            </div>
+            </div>
            
-        )
+        );
     }
 
 
