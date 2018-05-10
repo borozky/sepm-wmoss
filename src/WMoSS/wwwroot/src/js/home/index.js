1d949c048371a71a6ba4e7e784dbd8af499925ee
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-05-10 14:10:42 +1000

    Updated element id to 'ExpressBookingParent'

--- src/WMoSS/wwwroot/src/js/home/index.js
+++ src/WMoSS/wwwroot/src/js/home/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import ExpressBooking from "./ExpressBooking";
 
 /** @type {HTMLDivElement} */
-var expressCheckoutParent = document.getElementById("ExpressCheckoutParent");
+var expressBookingParent = document.getElementById("ExpressBookingParent");
 
 $(document).ready( function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-ReactDOM.render(<ExpressBooking/>, expressCheckoutParent)
+// prevents dropdown menu from closing when a seat is clicked
+$(document).on('click', '#ExpressBookingParent .dropdown-menu', function (e) {
+    e.stopPropagation();
+});
+
+ReactDOM.render(<ExpressBooking/>, expressBookingParent)
