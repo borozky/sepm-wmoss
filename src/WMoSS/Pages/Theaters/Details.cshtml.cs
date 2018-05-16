dd0e1df01391288cbdae070049524b3b0ce31963
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-05-16 23:48:38 +1000

    Implemented theater details page

--- src/WMoSS/Pages/Theaters/Details.cshtml.cs
+++ src/WMoSS/Pages/Theaters/Details.cshtml.cs
@@ -4,14 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WMoSS.Data;
+using WMoSS.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace WMoSS.Pages.Theaters
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext db;
 
+        public DetailsModel(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        
+        public IEnumerable<Movie> Movies { get; set; }
+        public Theater Theater { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Theater = await db.Theaters.FirstOrDefaultAsync(t => t.Id == id);
+            if (Theater == null)
+            {
+                return NotFound();
+            }
+
+            var movieIds = await db.MovieSessions
+                .AsNoTracking()
+                .Where(ms => ms.TheaterId == id)
+                .Select(ms => ms.MovieId)
+                .ToArrayAsync();
+                
+            var ids = movieIds.ToHashSet();
+            Movies = await db.Movies.Where(m => ids.Contains(m.Id)).ToListAsync();
+
+            return Page();
         }
     }
 }
